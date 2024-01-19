6fb9d90c994183bd03256b36453562dc29ec214d
Hannele Ruiz <justlemoncl@gmail.com>
2024-01-19 00:14:05 -0300

    Added vehicle spawning command

--- LeMi/Vehicles.cs
+++ LeMi/Vehicles.cs
@@ -1,5 +1,6 @@
 ﻿#if FIVEM
 using CitizenFX.Core;
+using System.Threading.Tasks;
 #elif SHVDN3
 using GTA;
 #endif
@@ -15,4 +16,44 @@ public static class Vehicles
     /// Deletes the current vehicle.
     /// </summary>
     public static void DeleteCurrent() => Game.Player.Character.CurrentVehicle?.Delete();
+    /// <summary>
+    /// Spawns a vehicle by it's model.
+    /// </summary>
+    /// <param name="model">The model of the vehicle.</param>
+    /// <param name="getInto">Whether to get the player into the vehicle or not.</param>
+    #if FIVEM
+    public static async Task<Vehicle> SpawnVehicle(Model model, bool getInto)
+    #elif SHVDN3
+    public static Vehicle SpawnVehicle(Model model, bool getInto)
+    #endif
+    {
+        if (!model.IsCar)
+        {
+            return null;
+        }
+
+        while (!model.IsLoaded)
+        {
+            model.Request();
+            
+            #if FIVEM
+            await BaseScript.Delay(0);
+            #elif SHVDN3
+            Script.Yield();
+            #endif
+        }
+
+        #if FIVEM
+        Vehicle vehicle = await World.CreateVehicle(model, Game.Player.Character.Position, Game.Player.Character.Heading);
+        #elif SHVDN3
+        Vehicle vehicle = World.CreateVehicle(model, Game.Player.Character.Position, Game.Player.Character.Heading);
+        #endif
+
+        if (getInto)
+        {
+            Game.Player.Character.SetIntoVehicle(vehicle, VehicleSeat.Driver);
+        }
+
+        return vehicle;
+    }
 }
