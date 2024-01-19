82846554505126347adaf7a9bda72ee5951896aa
Hannele Ruiz <justlemoncl@gmail.com>
2024-01-18 23:52:14 -0300

    Added tool to delete the current vehicle

---
+++ LeMi/Vehicles.cs
@@ -0,0 +1,18 @@
+﻿#if FIVEM
+using CitizenFX.Core;
+#elif SHVDN3
+using GTA;
+#endif
+
+namespace LeMi.Client;
+
+/// <summary>
+/// Tools for managing vehicles.
+/// </summary>
+public static class Vehicles
+{
+    /// <summary>
+    /// Deletes the current vehicle.
+    /// </summary>
+    public static void DeleteCurrent() => Game.Player.Character.CurrentVehicle?.Delete();
+}
