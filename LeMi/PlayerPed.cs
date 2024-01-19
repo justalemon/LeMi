59e4f8d50ebcbbb58ec4e8db145dec44459dbc5b
Hannele Ruiz <justlemoncl@gmail.com>
2024-01-19 01:47:29 -0300

    Added revive command

---
+++ LeMi/PlayerPed.cs
@@ -0,0 +1,30 @@
+﻿#if FIVEM
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+#elif SHVDN3
+using GTA;
+using GTA.Native;
+#endif
+
+namespace LeMi.Client;
+
+/// <summary>
+/// Class for handling the Player and it's Ped
+/// </summary>
+public static class PlayerPed
+{
+    /// <summary>
+    /// Revives the local player ped.
+    /// </summary>
+    public static void Revive()
+    {
+        Ped ped = Game.Player.Character;
+        
+        #if FIVEM
+        API.ResurrectPed(ped.Handle);
+        #elif SHVDN3
+        Function.Call(Hash.RESURRECT_PED, ped.Handle);
+        #endif
+        ped.Task.ClearAllImmediately();
+    }
+}
